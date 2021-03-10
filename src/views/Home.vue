8d1e579c0a8d57b8abbd2b53fbaa6881fa53d57b
Maciej Romanowski <neipreis22@gmail.com>
2021-03-10 21:03:13 +0100

    implemented apollo queries to home view with table and pagination components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,106 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <characters-table
+      :characters="charactersInfo || []"
+      :pagedResult="dataInfo || {}"
+    />
+    <table-pagination
+      v-if="dataInfo.pages > 1"
+      :pagedResult="dataInfo || {}"
+      :currentPage="currentPage"
+      @pageChanged="updateCurrentPage"
+    />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import CharactersTable from "@/components/CharactersTable.vue";
+import TablePagination from "@/components/TablePagination.vue";
+import CharactersQuery from "@/queries/ts/Characters";
+import CharacterByIdQuery from "@/queries/ts/CharacterById";
+import CharactersByEpisodeQuery from "@/queries/ts/CharactersByEpisode";
+import { Character, PagedResult } from "@/typescript";
 
 @Component({
   components: {
-    HelloWorld
+    CharactersTable,
+    TablePagination
+  },
+  apollo: {
+    characters: {
+      query() {
+        const { filterType } = this;
+        switch (filterType) {
+          case "Name":
+            return CharactersQuery;
+          case "Identifier":
+            return CharacterByIdQuery;
+          case "Episode":
+            return CharactersByEpisodeQuery;
+          default:
+            return CharactersQuery;
+        }
+      },
+      variables() {
+        return {
+          page: this.currentPage as number,
+          filterValue: this.filterValue as string | number
+        };
+      },
+      result({ data, loading }) {
+        const { filterType } = this;
+
+        if (!loading) {
+          switch (filterType) {
+            case "Name":
+              this.dataInfo = data.characters.info;
+              this.charactersInfo = data.characters.results;
+              break;
+            case "Identifier":
+              this.dataInfo = {
+                count: 0,
+                pages: 0,
+                next: 0,
+                prev: 0
+              };
+              this.charactersInfo = [data.character];
+              break;
+            case "Episode":
+              this.dataInfo = data.episodes.info;
+              this.charactersInfo = data.episodes.results[0].characters;
+              break;
+            default:
+              this.dataInfo = data.characters.info;
+              this.charactersInfo = data.characters.results;
+              break;
+          }
+        }
+      }
+    }
   }
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  currentPage = 1;
+
+  get filterType(): string {
+    return this.$store.getters["FILTER_TYPE"];
+  }
+
+  get filterValue(): string {
+    return this.$store.getters["FILTER_VALUE"];
+  }
+
+  dataInfo: PagedResult = {
+    count: 0,
+    pages: 0,
+    next: 0, // TOOD MAKE NULLABLE
+    prev: 0 // TOOD MAKE NULLABLE
+  };
+
+  charactersInfo: Character[] = [];
+  updateCurrentPage(newPage: number) {
+    this.currentPage = newPage;
+  }
+}
 </script>
