b61cafc0f0879e776543ecf79c4fd888f0e0bb7f
Maciej Romanowski <neipreis22@gmail.com>
2021-03-10 20:59:08 +0100

    Select component required v-model and items (options to select)

---
+++ src/components/SearchBarSelect.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="select" role="tree" @click="showOptions()">
+    <div class="select__selected">
+      <slot name="selected" :selected="value">
+        {{ value }}
+      </slot>
+    </div>
+    <slot name="icon">
+      <material-design-icon
+        class="select_icon"
+        :class="optionsMenu && 'select__icon-animation'"
+      >
+        arrow_left
+      </material-design-icon>
+    </slot>
+    <div class="options" role="presentation" v-if="optionsMenu">
+      <template v-for="(item, index) in items">
+        <div
+          :key="index"
+          class="options__item"
+          :class="item === value && 'options__item-selected'"
+          role="treeitem"
+          @click="selectItem(item)"
+        >
+          {{ item }}
+        </div>
+      </template>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+import MaterialDesignIcon from "@/components/MaterialDesignIcon.vue";
+
+@Component({
+  components: {
+    MaterialDesignIcon
+  }
+})
+export default class SearchBarSelect extends Vue {
+  @Prop({ required: true, default: [] }) items!: string[];
+  @Prop({ required: true, default: "" }) value!: string;
+
+  optionsMenu = false;
+
+  showOptions(): void {
+    this.optionsMenu = !this.optionsMenu;
+  }
+
+  selectItem(selectedItem: string): void {
+    this.$emit("input", selectedItem);
+  }
+}
+</script>
+
+<style>
+.select {
+  display: flex;
+  height: 100%;
+  padding: 8px;
+  flex-direction: row;
+  flex-grow: 1;
+  align-items: center;
+  position: relative;
+  user-select: none;
+  cursor: pointer;
+}
+.select__selected {
+  display: flex;
+  flex-grow: 1;
+  flex-shrink: 0;
+  justify-content: start;
+}
+.select_icon {
+  transition: all 0.23s ease-in-out;
+}
+.select__icon-animation {
+  transform: rotate(-90deg);
+}
+.options {
+  background-color: var(--background-color);
+  width: 100%;
+  box-sizing: content-box;
+  border: 1px solid var(--secondary-color);
+  border-bottom-left-radius: 8px;
+  border-bottom-right-radius: 8px;
+  position: absolute;
+  top: 100%;
+  left: -1px;
+  right: -1px;
+  z-index: 5;
+}
+.options__item {
+  line-height: 24px;
+  text-align: start;
+  padding: 16px;
+}
+.options__item-selected {
+  background-color: var(--primary-color);
+  color: var(--background-color);
+}
+.options__item:hover {
+  cursor: pointer;
+  background-color: var(--primary-color);
+  color: var(--background-color);
+}
+
+.options .options__item:nth-child(n) {
+  border-bottom: 1px solid var(--secondary-color);
+}
+.options .options__item:last-child {
+  border: none;
+  border-bottom-left-radius: 8px;
+  border-bottom-right-radius: 8px;
+}
+.options .options__item:last-child:hover {
+  border-bottom-left-radius: 8px;
+  border-bottom-right-radius: 8px;
+}
+</style>
