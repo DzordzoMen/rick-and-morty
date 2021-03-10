ef26d73c3aa78c7b05052204e985ad778d7bddbc
Maciej Romanowski <neipreis22@gmail.com>
2021-03-10 20:50:47 +0100

    added vue apollo lib

---
+++ apollo.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  client: {
+    service: {
+      name: 'my-app',
+      url: 'https://rickandmortyapi.com/graphql',
+    },
+    includes: [
+      'src/**/*.vue',
+      'src/**/*.ts',
+      'src/**/*.js',
+    ],
+  },
+};
