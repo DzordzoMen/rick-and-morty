77269bd3afef96a373eb6ea577289af9468ab748
Maciej Romanowski <neipreis22@gmail.com>
2021-03-11 12:23:56 +0100

    created container component with media queries

---
+++ src/components/Container.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component
+export default class Container extends Vue {}
+</script>
+
+<style>
+@media (min-width: 960px) {
+  .container {
+    max-width: 900px;
+  }
+}
+@media (min-width: 1264px) {
+  .container {
+    max-width: 1185px;
+  }
+}
+.container {
+  margin: auto;
+  width: 100%;
+}
+</style>
