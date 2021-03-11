079fac9446e4d8b3f04572319ec16d586cfb67b1
Maciej Romanowski <neipreis22@gmail.com>
2021-03-11 12:45:23 +0100

    created Favorites view which using table component and favorite module getter

---
+++ src/views/Favorites.vue
@@ -0,0 +1,36 @@
+<template>
+  <container>
+    <characters-table :characters="characters" :headers="tableHeaders" />
+  </container>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+import { Character } from "@/typescript";
+import Container from "@/components/Container.vue";
+import CharactersTable from "@/components/CharactersTable.vue";
+
+@Component({
+  components: {
+    Container,
+    CharactersTable
+  }
+})
+export default class Favorites extends Vue {
+  get characters(): Character[] {
+    return this.$store.getters["favorite/FAVORITE_CHARACTERS"];
+  }
+
+  tableHeaders: string[] = [
+    "Photo",
+    "Character ID",
+    "Name",
+    "Gender",
+    "Species",
+    "Last Episode",
+    "Add to Favorites"
+  ];
+}
+</script>
+
+<style></style>
